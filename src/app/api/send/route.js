0efc96265e7cd3d0455d322d29f7fd6d12e9638c
chiemgiabaost <bao2003@my.yorku.ca>
2025-01-04 00:16:42 -0500

    Fixing bugs for deployment

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,22 +1,22 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { EmailTemplate } from '../../components/EmailTemplate';
-import { Resend } from 'resend';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { EmailTemplate } from '../../components/EmailTemplate';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async (req, res) => {
-  const { data, error } = await resend.emails.send({
-    from: 'Gia <chiemgiabaost@gmail.com>',
-    to: ['delivered@resend.dev'],
-    subject: 'Hello world',
-    react: <>
-        <p>Email Body</p>
-    </>
-  });
+// export default async (req, res) => {
+//   const { data, error } = await resend.emails.send({
+//     from: 'Gia <chiemgiabaost@gmail.com>',
+//     to: ['delivered@resend.dev'],
+//     subject: 'Hello world',
+//     react: <>
+//         <p>Email Body</p>
+//     </>
+//   });
 
-  if (error) {
-    return res.status(400).json(error);
-  }
+//   if (error) {
+//     return res.status(400).json(error);
+//   }
 
-  res.status(200).json(data);
-};
+//   res.status(200).json(data);
+// };
